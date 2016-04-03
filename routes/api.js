857a4a4972e0aa7a0c39d2d03afe9abd070aa517
Jeff0002 <llx0002@gmail.com>
2016-04-03 01:45:59 -0400

    DB Test

--- routes/api.js
+++ routes/api.js
@@ -116,7 +116,7 @@ router.get('/shop/:username', function (req, res, next) {
 
 router.get('/test', function (req, res) {
     res.json({
-        "test": "success001"
+        "test": "success002"
     });
 });
 
